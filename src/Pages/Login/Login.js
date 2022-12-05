54dcdb4c904616d9568e647b510beb4bc7f2f428
md-rakibul-islam <raaakib0@gmail.com>
2022-12-06 01:22:18 +0600

    login add

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
+import SocialLogin from '../Shared/SocialLogin/SocialLogin';
 
 const Login = () => {
 
@@ -43,7 +44,7 @@ const Login = () => {
                         localStorage.setItem('genius-token', data.token);
                         navigate(from, { replace: true });
                     });
-                
+
             })
             .catch(error => console.log(error));
     }
@@ -77,6 +78,7 @@ const Login = () => {
                         </div>
                     </form>
                     <p className='text-center'>New to Genius Car <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link> </p>
+                    <SocialLogin></SocialLogin>
                 </div>
             </div>
         </div>
