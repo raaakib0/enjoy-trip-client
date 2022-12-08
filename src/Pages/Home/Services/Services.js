68539f3bc883c721cfacd30dfa2388c386134ad0
md-rakibul-islam <raaakib0@gmail.com>
2022-12-08 11:09:14 +0600

    service and order database coonnection

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -9,13 +9,13 @@ const Services = () => {
     const [isAsc, setIsAsc] = useState(true);
     const [search, setSearch] = useState('');
     const searchRef = useRef();
-    useEffect( () =>{
-        fetch(`http://localhost:5000/services?search=${search}&order=${ isAsc ? 'asc' : 'desc'}`)
-        .then(res =>res.json())
-        .then(data => setServices(data))
+    useEffect(() => {
+        fetch(`http://localhost:5000/services?search=${search}&order=${isAsc ? 'asc' : 'desc'}`)
+            .then(res => res.json())
+            .then(data => setServices(data))
     }, [isAsc, search]);
 
-    const handleSearch = () =>{
+    const handleSearch = () => {
         setSearch(searchRef.current.value);
     }
 
@@ -25,7 +25,7 @@ const Services = () => {
                 <p className="text-2xl font-bold text-orange-600">Services</p>
                 <h2 className="text-5xl font-semibold">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
-                <input className='input input-sm' ref={searchRef} type="text" /> 
+                <input className='input input-sm' ref={searchRef} type="text" />
                 <button onClick={handleSearch}>Search</button>
                 <button className='btn btn-ghost' onClick={() => setIsAsc(!isAsc)}>{isAsc ? 'desc' : 'asc'}</button>
             </div>
@@ -42,3 +42,40 @@ const Services = () => {
 };
 
 export default Services;
+
+// import { data } from 'autoprefixer';
+// import React from 'react';
+// import { useEffect } from 'react';
+// import { useState } from 'react';
+// import ServiceCard from './ServiceCard';
+
+// const Services = () => {
+//     const [services, setServices] = useState([]);
+
+//     useEffect(() => {
+//         fetch('services.json')
+//             .then(res => res.json())
+//             .then(data => setServices(data))
+//     }, []);
+
+//     return (
+//         <div>
+//             <div className='text-center mb-4'>
+//                 <p className="text-2xl font-bold text-orange-600">Services</p>
+//                 <h2 className="text-5xl font-semibold">Our Service Area</h2>
+//                 <h2>data: {services.length}</h2>
+//                 <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+//             </div>
+//             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+//                 {
+//                     services.map(service => <ServiceCard
+//                         key={service._id}
+//                         service={service}
+//                     ></ServiceCard>)
+//                 }
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Services;
